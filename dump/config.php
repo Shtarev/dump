4cd6d656b90f8badcd4ed82296491ca8af11c056
Andrey Shtarev <54120707+Shtarev@users.noreply.github.com>
2020-04-20 11:04:10 +0200

    Update config.php

--- dump/config.php
+++ dump/config.php
@@ -2,5 +2,6 @@
 // Initialize $style аny of following values: dreamweaver, sunburst, doxy, desert, sons-of-obsidian
 $style = "dreamweaver";
 ?>
-<script src="<?=substr(str_replace('\\','/',__DIR__), strlen($_SERVER['DOCUMENT_ROOT']))?>/src/prettify/run_prettify.js?autoload=true&amp;skin=<?=$style?>&amp;lang=css" defer></script>
+<script src="<?=substr(str_replace('\\','/',__DIR__), strlen($_SERVER['DOCUMENT_ROOT']))?>/src/prettify/run_prettify.js" defer></script>
+<link href="<?=substr(str_replace('\\','/',__DIR__), strlen($_SERVER['DOCUMENT_ROOT']))?>/src/prettify/<?=$style?>.css" rel="stylesheet" type="text/css">
 <link href="<?=substr(str_replace('\\','/',__DIR__), strlen($_SERVER['DOCUMENT_ROOT']))?>/src/style.css" rel="stylesheet" type="text/css">
