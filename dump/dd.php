4f650d10042d6ae847db03ff060f29d506c2bcdf
Andrey Shtarev <54120707+Shtarev@users.noreply.github.com>
2022-07-15 14:28:05 +0300

    Update dd.php

--- dump/dd.php
+++ dump/dd.php
@@ -1,26 +1,38 @@
 <?php
-/** PHP-Programmierung by Andrey Shtarev © www.shtarev.com
+/** PHP-Programmierung by Andrey Shtarev © https://github.com/Shtarev
 * Drop array, objects, variable or scripting code.
 * Visual:this script in part used a theme code-prettify.
 */
 require (__DIR__.'/config.php');
 
-function dd($var, $die = false, $file = __FILE__, $line = __LINE__){
-	if(is_array($var) || is_object($var)){
-		if($die==false){
-			echo ("<pre class=\"prettyprint\" id=\"quine\">".print_r($var, true)."<hr>Файл — ".$file."<br>Строка — ".$line."</pre>");
+function dd($var, $die = false) {
+	$backtrace = debug_backtrace();
+	$file = $backtrace[0]['file'];
+	$line = $backtrace[0]['line'];
+
+	if(is_array($var) || is_object($var)) {
+		if($die == false) {
+			echo ("<pre class=\"prettyprint\" id=\"quine\">".print_r($var, true)."<hr>Файл — ".$file."<br>Строка — ".$line."<center>
+			Copyright © 2010<script>new Date().getFullYear()>2010&&document.write('-'+new Date().getFullYear());</script> | Freiheitsgewährende Software | Design & programmierung by <a href='https://github.com/Shtarev'>Andrey Shtarev</a>
+			</center></pre>");
 		} 
 		else {
-			die ("<pre class=\"prettyprint\" id=\"quine\">".print_r($var, true)."<hr>Файл — ".$file."<br>Строка — ".$line."</pre>");
+			die ("<pre class=\"prettyprint\" id=\"quine\">".print_r($var, true)."<hr>Файл — ".$file."<br>Строка — ".$line."<center>
+			Copyright © 2010<script>new Date().getFullYear()>2010&&document.write('-'+new Date().getFullYear());</script> | Freiheitsgewährende Software | Design & programmierung by <a href='https://github.com/Shtarev'>Andrey Shtarev</a>
+			</center></pre>");
 		}
 	}
-	else{
+	else {
 		$var = htmlspecialchars($var);
-		if($die==false){
-			echo ("<pre class=\"prettyprint\" id=\"quine\">".$var."<hr>Файл — ".$file."<br>Строка — ".$line."</pre>");
+		if($die == false) {
+			echo ("<pre class=\"prettyprint\" id=\"quine\">".$var."<hr>Файл — ".$file."<br>Строка — ".$line."<center>
+			Copyright © 2010<script>new Date().getFullYear()>2010&&document.write('-'+new Date().getFullYear());</script> | Freiheitsgewährende Software | Design & programmierung by <a href='https://github.com/Shtarev'>Andrey Shtarev</a>
+			</center></pre>");
 		}
-		else{
-			die ("<pre class=\"prettyprint\" id=\"quine\">".$var."<hr>Файл — ".$file."<br>Строка — ".$line."</pre>");
+		else {
+			die ("<pre class=\"prettyprint\" id=\"quine\">".$var."<hr>Файл — ".$file."<br>Строка — ".$line."<center>
+			Copyright © 2010<script>new Date().getFullYear()>2010&&document.write('-'+new Date().getFullYear());</script> | Freiheitsgewährende Software | Design & programmierung by <a href='https://github.com/Shtarev'>Andrey Shtarev</a>
+			</center></pre>");
 		}
 	}
 }
